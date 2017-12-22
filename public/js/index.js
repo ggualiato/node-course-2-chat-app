72b878a94d817ba6288537e6908239e45dc6dcab
ggualiato <ggualiato@gmail.com>
2017-12-21 23:33:40 -0200

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -29,11 +29,13 @@ socket.on('newLocationMessage', function (message) {
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault()
 
+  var messageTextBox = jQuery('[name=message]')
+
   socket.emit('createMessage', {
     from: 'User',
-    text: jQuery('[name=message]').val()
+    text: messageTextBox.val()
   }, function () {
-
+    messageTextBox.val('')
   })
 })
 
@@ -44,7 +46,12 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser')
   }
 
+  locationButton.attr('disabled', 'disabled').text('Sending location...')
+
   navigator.geolocation.getCurrentPosition(function (position) {
+
+    locationButton.removeAttr('disabled').text('Send location')
+
     socket.emit('createLocationMessage', {
       latitude: position.coords.latitude,
       longitude: position.coords.longitude
--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ io.on('connection', (socket) => {
 
     io.emit('newMessage', generateMessage(message.from, message.text))
 
-    callback('This is from the server')
+    callback()
   })
 
   socket.on('createLocationMessage', (coords) => {
