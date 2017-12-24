98e40d09c90d52e120b4bc6c437dab58d10375b9
ggualiato <ggualiato@gmail.com>
2017-12-24 01:22:46 -0200

    Scroll to bottom if user is close to bottom

--- server/server.js
+++ server/server.js
@@ -20,8 +20,6 @@ io.on('connection', (socket) => {
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
   socket.on('createMessage', (message, callback) => {
-    console.log('Message', message)
-
     io.emit('newMessage', generateMessage(message.from, message.text))
 
     callback()
